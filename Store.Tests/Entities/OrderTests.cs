72a6dfe6b3d85b561cac6ea43923503506cdd630
Daniel Martins <freitasdaniel39@gmail.com>
2022-02-06 09:04:47 -0300

    Testando um desconto valido

--- Store.Tests/Entities/OrderTests.cs
+++ Store.Tests/Entities/OrderTests.cs
@@ -76,4 +76,22 @@ public class OrderTests
         order.AddItem(_product, 6); // valor do pedido = 60
         Assert.AreEqual(order.Total(), 60);
     }
+
+    [TestMethod]
+    [TestCategory("Domain")]
+    public void Dado_um_desconto_invalido_o_valor_do_pedido_deve_ser_60() {
+        var order = new Order(_customer, 0, null);
+        order.AddItem(_product, 6); // valor do pedido = 60
+        Assert.AreEqual(order.Total(), 60);
+    }
+
+    [TestMethod]
+    [TestCategory("Domain")]
+    public void Dado_um_desconto_de_10_o_valor_do_pedido_deve_ser_60() {
+        var order = new Order(_customer, 0, _discount); // - 10 de desconto + 0 de entrega = -10
+        order.AddItem(_product, 6); // valor do pedido = 60 - 10 de desconto = 50
+        Assert.AreEqual(order.Total(), 50);
+    }
+
+    
 }
